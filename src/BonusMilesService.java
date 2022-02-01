793b241e1e4a3f70d94885b4208efb6ddaf5f46e
Yun Sofia <sonyayun@yandex.ru>
2022-02-01 15:38:33 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int miles = cost / 20;
+        return miles;
+
+    }
+}
+//public class Main {
+//    public static void main(String[] args) {
+//        BonusMilesService service = new BonusMilesService();
+//        int price = 10_000;
+//        int miles = service.calculate(price);
+//        System.out.println(miles);
